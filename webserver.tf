8682b810e2456180e5c57926555d129baca8feb6
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2019-08-13 21:51:39 +0530

    Update webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -80,14 +80,7 @@ echo '########## basic webserver ##############'
 yum install -y httpd
 systemctl enable  httpd.service
 systemctl start  httpd.service
-echo '<html><head></head><body><pre><code>' > /var/www/html/index.html
-each '' >> /var/www/html/index.html
-echo '<H1><p style="color:red;">' >> /var/www/html/index.html
-hostname >> /var/www/html/index.html
-echo '</p></H1>' >> /var/www/html/index.html
-echo '<p>' >> /var/www/html/index.html
-echo '<img src="http://bit.ly/2NBa8MA" alt="OOW2018" align="left">' >> /var/www/html/index.html
-echo '</code></pre></body></html>' >> /var/www/html/index.html
+echo '<!DOCTYPE html><html><head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><style>.card {box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px; margin: auto; text-align: center; font-family: arial;}.title {  color: grey;  font-size: 18px;}button {  border: none;  outline: 0;  display: inline-block;  padding: 8px;  color: white;  background-color: #000;  text-align: center;  cursor: pointer;  width: 100%;  font-size: 18px;}a {  text-decoration: none;  font-size: 22px;  color: black;}button:hover, a:hover {  opacity: 0.7;}</style></head><body><h2 style="text-align:center">User Profile Card</h2><div class="card"><img src="https://drive.google.com/uc?id=1J9S95FtwefSF7hlscpsUVOYhv9Vr_AyZ&export=download" alt="Pic" style="width:100%"><h1>John Doe</h1> <p class="title">CUSTOMER</p>  <p>john.doe@xyz.com</p>  <div style="margin: 24px 0;"><a href="#"><i class="fa fa-dribbble"></i></a><a href="#"><i class="fa fa-twitter"></i></a>  <a href="#"><i class="fa fa-linkedin"></i></a>  <a href="#"><i class="fa fa-facebook"></i></a></div><p><button>Contact</button></p></div></body></html>' >> /var/www/html/index.html
 firewall-offline-cmd --add-service=http
 systemctl enable  firewalld
 systemctl restart  firewalld
